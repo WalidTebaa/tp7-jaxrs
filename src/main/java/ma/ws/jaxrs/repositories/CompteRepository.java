318d7144ee1b2791ffb857565c6e4e411795320b
WalidTebaa <walidtebaa20@gmail.com>
2025-10-29 21:25:00 +0100

    Initial commit

---
+++ src/main/java/ma/ws/jaxrs/repositories/CompteRepository.java
@@ -0,0 +1,9 @@
+package ma.ws.jaxrs.repositories;
+
+import ma.ws.jaxrs.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
